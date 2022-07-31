4f4f5cbe62c511ba7d4f9559cc04cf309dadbfbd
Lokesh-Amara <amaralokesh24@gmail.com>
2022-07-31 17:19:29 +0530

    new version

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
 const axios = require("axios");
 
 const genRand = (len) => {
-  //   var resp = await axios.get(
-  //     "https://baconipsum.com/api/?type=all-meat&sentences=1&start-with-lorem=1"
-  //   );
-  //   console.log(resp.data);
-
   axios
     .get(
       "https://baconipsum.com/api/?type=all-meat&sentences=1&start-with-lorem=1"
     )
-    .then((res) => console.log(res.data));
-
-  return Math.random()
-    .toString(36)
-    .substring(2, len + 2);
+    .then((res) => {
+      return res.data;
+    });
 };
 
-console.log(genRand(6));
 module.exports = genRand;
