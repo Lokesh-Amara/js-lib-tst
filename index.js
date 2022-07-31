dbb0bdb9b872d8734d41042eef5eebd2d24ab0d6
Lokesh-Amara <amaralokesh24@gmail.com>
2022-07-31 17:37:56 +0530

    new version 2

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const genRand = (len) => {
     .get(
       "https://baconipsum.com/api/?type=all-meat&sentences=1&start-with-lorem=1"
     )
-    .then((res) => {
-      return res.data;
-    });
+    .then((res) => console.log(res.data));
+
+  return Math.random()
+    .toString(36)
+    .substring(2, len + 2);
 };
 
 module.exports = genRand;
